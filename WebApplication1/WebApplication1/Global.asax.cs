f10bfb8afa5fede0cfb6e75fdf325d3033fe6ad7
Maximilian De Oliveira-Spiker <s3476696@student.rmit.edu.au>
2015-09-17 13:24:34 +1000

    test

--- WebApplication1/WebApplication1/Global.asax.cs
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-//nkjsdjdbvjsdbvkj
+
 namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
