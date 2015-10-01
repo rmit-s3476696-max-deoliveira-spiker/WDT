3ad5642e54483e9202b8a2461293fbd4e34e94a8
Kevin Ivander <s3481736@student.rmit.edu.au>
2015-10-01 16:38:26 +1000

    Fix Up Movie class.

--- WebApplication1/WebApplication1/Models/Movie.cs
+++ WebApplication1/WebApplication1/Models/Movie.cs
@@ -4,14 +4,14 @@ namespace WebApplication1.Models
 {
     public class Movie
     {
-        // Test new commit
-        // this is a test.
         public int Movie_ID { get; set; }
         public string Movie_Title { get; set; }
-        public string Dsecription { get; set; }
+        public string Short_Description { get; set; }
+        public string Long_Description { get; set; }
         public string Poster { get; set; }
         public int Duration { get; set; }
         public char Rating { get; set; }
+        public double Price { get; set; }
     }
     public class MovieDBContext : DbContext
     {
