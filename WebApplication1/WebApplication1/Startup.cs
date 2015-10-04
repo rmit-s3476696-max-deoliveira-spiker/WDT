8a4d54e56097a7735b9112175542b5fbd8753dc1
Maximilian De Oliveira-Spiker <s3476696@student.rmit.edu.au>
2015-10-04 16:16:08 +1100

    UCW

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -13,5 +13,12 @@ namespace WebApplication1
         }
 
         // Testing -Commits-
+        // I am the bone of my code
+        // C# is my body and ASP.NET is my blood
+        // I have written over a thousand lines
+        // Unknown to rest, not known to social life
+        // Have withstood pain to create many programs
+        // Yet those hands will never hold anything (probably due to carpal tunnel syndrome)
+        // So as I pray, Unlimited Code Works
     }
 }
