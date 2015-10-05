c4d429030c1452dc8e5eee2c548ba2d94fa22c4e
Kevin Ivander <s3481736@student.rmit.edu.au>
2015-10-05 17:50:51 +1100

    Home Page v2

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -6,29 +6,9 @@ namespace WebApplication1
 {
     public partial class Startup
     {
-        // this ...
-        // this should be work
-        // see this?
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
         }
-
-        // Testing -Commits-
-        // I am the bone of my code
-        // C# is my body and ASP.NET is my blood
-        // I have written over a thousand lines
-        // Unknown to rest, not known to social life
-        // Have withstood pain to create many programs
-        // Yet those hands will never hold anything (probably due to carpal tunnel syndrome)
-        // So as I pray, Unlimited Code Works
-
-
-
-        /*
-         
-         Commit test
-         
-         */
     }
 }
